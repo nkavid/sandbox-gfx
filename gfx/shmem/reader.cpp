38ca2f5b240b7ff6dd661c3f553fd7e5913e2079
Martin Divak <74562922+nkavid@users.noreply.github.com>
2023-05-14 21:45:35 +0200

    shmem: more specific suppresions

--- gfx/shmem/reader.cpp
+++ gfx/shmem/reader.cpp
@@ -9,30 +9,29 @@
 #include <cstring>
 #include <string>
 
-/*
-  NOLINTBEGIN(cppcoreguidelines-prefer-member-initializer,
-              cppcoreguidelines-pro-type-vararg,
-              hicpp-vararg)
-*/
-
 namespace gfx::shmem
 {
 Reader::Reader(const char* name, size_t size)
     : _name{name},
       _size{size}
 {
+  // NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
   _fd = shm_open(_name.c_str(), O_CREAT | O_RDONLY, S_IRWXU);
   detail::checkForError(_fd, "Reader::shm_open");
 
+  // NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
   _address = mmap(nullptr, _size, PROT_READ, MAP_SHARED, _fd, 0);
   detail::checkForError(_address, "Reader::mmap");
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreMutex = sem_open((_name + "_mutex").c_str(), O_CREAT | O_RDWR, S_IRWXU, 1);
   detail::checkForError(_semaphoreMutex, "Writer::sem_open", SEM_FAILED);
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreEmpty = sem_open((_name + "_empty").c_str(), O_CREAT | O_RDWR, S_IRWXU, 1);
   detail::checkForError(_semaphoreEmpty, "Writer::sem_open", SEM_FAILED);
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreFull = sem_open((_name + "_full").c_str(), O_CREAT | O_RDWR, S_IRWXU, 0);
   detail::checkForError(_semaphoreFull, "Writer::sem_open", SEM_FAILED);
 }
@@ -50,9 +49,3 @@ Reader::Reader(const char* name, size_t size)
   return false;
 }
 }
-
-/*
-  NOLINTEND(cppcoreguidelines-prefer-member-initializer,
-            cppcoreguidelines-pro-type-vararg,
-            hicpp-vararg)
-*/
--- gfx/shmem/writer.cpp
+++ gfx/shmem/writer.cpp
@@ -10,31 +10,30 @@
 #include <cstring>
 #include <string>
 
-/*
-  NOLINTBEGIN(cppcoreguidelines-prefer-member-initializer,
-              cppcoreguidelines-pro-type-vararg,
-              hicpp-vararg)
-*/
-
 namespace gfx::shmem
 {
 Writer::Writer(const char* name, size_t size)
     : _name{name},
       _size{size}
 {
+  // NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
   _fd = shm_open(_name.c_str(), O_CREAT | O_RDWR, S_IRWXU);
   detail::checkForError(_fd, "Writer::shm_open");
   detail::checkForError(ftruncate(_fd, static_cast<off_t>(_size)), "Writer::ftruncate");
 
+  // NOLINTNEXTLINE(cppcoreguidelines-prefer-member-initializer)
   _address = mmap(nullptr, _size, PROT_WRITE, MAP_SHARED, _fd, 0);
   detail::checkForError(_address, "Writer::mmap");
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreMutex = sem_open((_name + "_mutex").c_str(), O_CREAT | O_RDWR, S_IRWXU, 1);
   detail::checkForError(_semaphoreMutex, "Writer::sem_open", SEM_FAILED);
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreEmpty = sem_open((_name + "_empty").c_str(), O_CREAT | O_RDWR, S_IRWXU, 1);
   detail::checkForError(_semaphoreEmpty, "Writer::sem_open", SEM_FAILED);
 
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
   _semaphoreFull = sem_open((_name + "_full").c_str(), O_CREAT | O_RDWR, S_IRWXU, 0);
   detail::checkForError(_semaphoreFull, "Writer::sem_open", SEM_FAILED);
 }
@@ -58,9 +57,3 @@ void Writer::write(void* data)
   }
 }
 }
-
-/*
-  NOLINTEND(cppcoreguidelines-prefer-member-initializer,
-            cppcoreguidelines-pro-type-vararg,
-            hicpp-vararg)
-*/
