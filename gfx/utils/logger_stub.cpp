9fca666f77248d819456bf846c955d693764a787
Martin Divak <74562922+nkavid@users.noreply.github.com>
2023-05-20 09:49:27 +0200

    remove dead code from logger stub

--- gfx/utils/logger_stub.cpp
+++ gfx/utils/logger_stub.cpp
@@ -7,13 +7,11 @@ namespace gfx::utils::logger
 void fatal(std::string_view /*msg*/)
 {
   throw std::runtime_error("called [[noreturn]] stub");
-  std::abort();
 }
 
 void fatal(std::string_view /*msg*/, std::string_view /*info*/)
 {
   throw std::runtime_error("called [[noreturn]] stub");
-  std::abort();
 }
 
 void error(std::string_view /*msg*/) {}
