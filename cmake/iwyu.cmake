96ae5ca3dc5d0b55b6245e2d8a26eb8de85aee3f
Martin Divak <74562922+nkavid@users.noreply.github.com>
2023-06-09 20:59:30 +0200

    always run iwyu by default and move mappings from cmake to tools

--- cmake/iwyu.cmake
+++ cmake/iwyu.cmake
@@ -4,7 +4,7 @@ set(IWYU_EXTERNAL_PATH /opt/pkg/llvm/llvm_16.0.5/share/include-what-you-use)
 
 set(GFX_IWYU_OPTIONS
     # -Xiwyu;--verbose=7;
-    # -Xiwyu;--no_comments;
+    -Xiwyu;--no_comments;
     -w;
     -Xiwyu;--keep=*GL/glew.h;
     -Xiwyu;--keep=*libavutil/timestamp.h;
@@ -19,8 +19,8 @@ set(GFX_IWYU_OPTIONS
     -Xiwyu;--mapping_file=${IWYU_EXTERNAL_PATH}/libcxx.imp;
     -Xiwyu;--mapping_file=${IWYU_EXTERNAL_PATH}/stl.c.headers.imp;
     -Xiwyu;--mapping_file=${IWYU_EXTERNAL_PATH}/boost-all.imp;
-    -Xiwyu;--mapping_file=${CMAKE_CURRENT_LIST_DIR}/glm.imp;
-    -Xiwyu;--mapping_file=${CMAKE_CURRENT_LIST_DIR}/hotfix.imp;
+    -Xiwyu;--mapping_file=${CMAKE_SOURCE_DIR}/tools/glm.imp;
+    -Xiwyu;--mapping_file=${CMAKE_SOURCE_DIR}/tools/hotfix.imp;
 )
 
 set(GFX_IWYU_CATCH2_TARGET_PROPERTIES
@@ -29,15 +29,5 @@ set(GFX_IWYU_CATCH2_TARGET_PROPERTIES
     -Xiwyu;--keep=*_stream_operator.hpp;
 )
 
-if(GFX_LINTING)
-  message(STATUS "gfx::ENABLED include-what-you-use")
-  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
-      ${GFX_IWYU_EXECUTABLE}; ${GFX_IWYU_OPTIONS};
-  )
-  set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${GFX_IWYU_EXECUTABLE}; ${GFX_IWYU_OPTIONS};)
-else()
-  message(STATUS "gfx::DISABLED include-what-you-use")
-  unset(CMAKE_CXX_CLANG_TIDY)
-  unset(CMAKE_CXX_INCLUDE_WHAT_YOU_USE)
-  set(GFX_IWYU_CATCH2_TARGET_PROPERTIES "")
-endif()
+set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${GFX_IWYU_EXECUTABLE}; ${GFX_IWYU_OPTIONS};)
+set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${GFX_IWYU_EXECUTABLE}; ${GFX_IWYU_OPTIONS};)
