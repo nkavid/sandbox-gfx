63f89e26bdb11649681d3b2972bd5db990192cce
Martin Divak <74562922+nkavid@users.noreply.github.com>
2023-06-17 17:48:39 +0200

    install option to gfx library cmake func

--- cmake/gfx_static_library_target.cmake
+++ cmake/gfx_static_library_target.cmake
@@ -1,6 +1,6 @@
 # Create GFX library target
 function(gfx_static_library_target)
-  set(options)
+  set(options INSTALL)
   set(one_value_args TARGET NAMESPACE INTERFACE_HEADERS)
   set(multi_value_args SOURCES DIRECTORIES DEPENDENCIES)
   cmake_parse_arguments(
@@ -25,10 +25,12 @@ function(gfx_static_library_target)
     ${GFX_LIBRARY_NAMESPACE}::${GFX_LIBRARY_TARGET} ALIAS ${target_name}
   )
 
-  install(TARGETS ${target_name} EXPORT gfxTargets)
+  if(${GFX_LIBRARY_INSTALL})
+    install(TARGETS ${target_name} EXPORT gfxTargets)
 
-  install(
-    FILES ${GFX_LIBRARY_INTERFACE_HEADERS}
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/gfx/${GFX_LIBRARY_NAMESPACE}"
-  )
+    install(
+      FILES ${GFX_LIBRARY_INTERFACE_HEADERS}
+      DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/gfx/${GFX_LIBRARY_NAMESPACE}"
+    )
+  endif()
 endfunction()
